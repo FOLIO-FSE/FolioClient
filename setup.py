16d7fdcbb417a6aebeca51514811a035029aea81
Theodor Tolstoy <ttolstoy@ebsco.com>
2020-05-07 08:54:44 +0200

    next version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name="folioclient",
     packages=["folioclient"],
-    version="0.21",
+    version="0.22",
     license="MIT",
     long_description="",
     long_description_content_type="text/markdown",
