8f98e62f81a0a0c1c3d0f58c33389f81f93549ed
Theodor Tolstoy <ttolstoy@ebsco.com>
2021-11-08 15:42:59 +0100

    long description

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from distutils.core import setup
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="folioclient",
     packages=["folioclient"],
-    version="0.31",
+    version="0.32",
     license="MIT",
-    long_description="file: README.md",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     description="A simple wrapper over the FOLIO LMS system API:s",
     author="Theodor Tolstoy",
     author_email="pypi.teddes@tolstoy.se",
     url="https://github.com/FOLIO-FSE/FolioClient/",
-    download_url="https://github.com/FOLIO-FSE/FolioClient/archive/v_031.tar.gz",
+    download_url="https://github.com/FOLIO-FSE/FolioClient/archive/v_032.tar.gz",
     keywords=["FOLIO", "FOLIO_LSP", "OKAPI", "API Wrapper"],
     install_requires=["requests"],  # I get to this in a second
     classifiers=[
