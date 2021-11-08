e973ca38bdb2e51ce4d15d4a3b023fe6b95fc57f
Theodor Tolstoy <ttolstoy@ebsco.com>
2021-11-08 22:30:33 +0100

    first stab at deterministic ids

--- folioclient/tests/test_basic.py
+++ folioclient/tests/test_basic.py
@@ -1,7 +1,15 @@
 # content of test_sample.py
-def func(x):
-    return x + 1
+from uuid import uuid5
+from determid.determid import DetermId
+
+from determid.folio_namespaces import FOLIONamespaces
 
 
-def test_answer2():
-    assert func(4) == 5
+def test_deterministic_uuids_namespace_generation():
+    u1 = uuid5(DetermId.base_namespace, "items")
+    assert str(u1) == str(FOLIONamespaces.items.value)
+
+
+def test_deterministic_uuid_generation():
+    u1 = DetermId("fs00001", FOLIONamespaces.items, "test")
+    assert "8eec379a-c007-5911-a2a8-4ba460ecc2e3" == str(u1)
