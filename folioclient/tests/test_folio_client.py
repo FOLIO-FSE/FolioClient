68861d0bb951754a92ffc6e16e9ae395fa0f76d1
Theodor Tolstoy <github.teddes@tolstoy.se>
2022-09-07 09:16:54 +0200

    move tests from folio_migration_tools

--- folioclient/tests/test_folio_client.py
+++ folioclient/tests/test_folio_client.py
@@ -1,6 +1,6 @@
-from unittest.mock import Mock
-from folioclient import FolioClient
 import pytest
+from folioclient import FolioClient
+from requests.exceptions import HTTPError
 
 
 def test_first():
@@ -30,3 +30,27 @@ def test_get_json_schema():
         "folio-org", "mod-user-import", "ramls/schemas/userdataimport.json"
     )
     assert json["properties"]
+
+
+def test_get_latest_from_github_returns_none_when_failing():
+    with pytest.raises(HTTPError):
+        FolioClient.get_latest_from_github("branchedelac", "tati", "myfile.json")
+
+
+def test_get_latest_from_github_returns_file_1():
+    schema = FolioClient.get_latest_from_github(
+        "folio-org",
+        "mod-source-record-manager",
+        "/mod-source-record-manager-server/src/main/resources/rules/marc_holdings_rules.json",
+    )
+    assert schema is not None
+    assert schema.get("001", None) is not None
+
+
+def test_get_latest_from_github_returns_file_orgs_has_no_releases():
+    with pytest.raises(HTTPError):
+        FolioClient.get_latest_from_github(
+            "folio-org",
+            "acq-models",
+            "/mod-orgs/schemas/organization.json",
+        )
