fcbf07450513b426c739841f681bc73b190e6c3d
Clinton Bradford <cbradford@ebsco.com>
2024-04-18 22:01:10 -0700

    refactor for code duplication lint

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -6,6 +6,18 @@ import httpx
 import unittest
 from types import SimpleNamespace
 
+acceptable_errors_side_effect = [
+    httpx.HTTPStatusError("error 502", request=None, response=SimpleNamespace(status_code=502)),
+    httpx.HTTPStatusError("error 503", request=None, response=SimpleNamespace(status_code=503)),
+    httpx.HTTPStatusError("error 504", request=None, response=SimpleNamespace(status_code=504)),
+    "test",
+]
+
+all_errors_side_effect = acceptable_errors_side_effect.copy()
+all_errors_side_effect.insert(
+    3, httpx.HTTPStatusError("error 500", request=None, response=SimpleNamespace(status_code=500))
+)
+
 
 @unittest.mock.patch("time.sleep", return_value=None)
 @unittest.mock.patch.dict("os.environ", {})
@@ -65,43 +77,12 @@ def test_handles_single_fail(_):
 @unittest.mock.patch("time.sleep", return_value=None)
 @unittest.mock.patch.dict(
     "os.environ",
-    {"FOLIOCLIENT_MAX_SERVER_ERROR_RETRIES": "3"},
+    {"FOLIOCLIENT_MAX_SERVER_ERROR_RETRIES": "5"},
 )
-def test_handles_double_fail(_):
+def test_handles_multiple_failures(_):
 
     internal_fn = unittest.mock.Mock(
-        return_value="test",
-        side_effect=[
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            "test",
-        ],
-    )
-
-    result = retry_on_server_error(internal_fn)()
-
-    assert internal_fn.call_count == 3
-    assert time.sleep.call_count == 2
-    assert time.sleep.call_args_list[0][0][0] == 10
-    assert time.sleep.call_args_list[1][0][0] == 30
-    assert result == internal_fn.return_value
-
-
-@unittest.mock.patch("time.sleep", return_value=None)
-@unittest.mock.patch.dict(
-    "os.environ",
-    {"FOLIOCLIENT_MAX_SERVER_ERROR_RETRIES": "3"},
-)
-def test_handles_triple_fail(_):
-
-    internal_fn = unittest.mock.Mock(
-        return_value="test",
-        side_effect=[
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            "test",
-        ],
+        return_value="test", side_effect=acceptable_errors_side_effect
     )
 
     result = retry_on_server_error(internal_fn)()
@@ -118,7 +99,7 @@ def test_handles_triple_fail(_):
 @unittest.mock.patch.dict(
     "os.environ",
     {
-        "FOLIOCLIENT_MAX_SERVER_ERROR_RETRIES": "3",
+        "FOLIOCLIENT_MAX_SERVER_ERROR_RETRIES": "4",
         "FOLIOCLIENT_SERVER_ERROR_RETRY_FACTOR": "5",
         "FOLIOCLIENT_SERVER_ERROR_RETRY_DELAY": "2",
     },
@@ -127,12 +108,7 @@ def test_handles_environment_variables(_):
 
     internal_fn = unittest.mock.Mock(
         return_value="test",
-        side_effect=[
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            "test",
-        ],
+        side_effect=acceptable_errors_side_effect,
     )
 
     result = retry_on_server_error(internal_fn)()
@@ -156,21 +132,15 @@ def test_handles_failthrough_on_tries(_):
 
     internal_fn = unittest.mock.Mock(
         return_value="test",
-        side_effect=[
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            "test",
-        ],
+        side_effect=all_errors_side_effect,
     )
 
-    with pytest.raises(httpx.HTTPStatusError):
+    with pytest.raises(httpx.HTTPStatusError) as e:
         retry_on_server_error(internal_fn)()
 
     assert internal_fn.call_count == 3
     assert time.sleep.call_count == 2
-    assert time.sleep.call_args_list[0][0][0] == 10
-    assert time.sleep.call_args_list[1][0][0] == 30
+    assert e.value.response.status_code == 504
 
 
 @unittest.mock.patch("time.sleep", return_value=None)
@@ -182,16 +152,7 @@ def test_handles_failthrough_on_tries(_):
 )
 def test_handles_failthrough_on_types(_):
 
-    internal_fn = unittest.mock.Mock(
-        return_value="test",
-        side_effect=[
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=503)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=504)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=500)),
-            "test",
-        ],
-    )
+    internal_fn = unittest.mock.Mock(return_value="test", side_effect=all_errors_side_effect)
 
     with pytest.raises(httpx.HTTPStatusError) as e:
         retry_on_server_error(internal_fn)()
@@ -199,31 +160,3 @@ def test_handles_failthrough_on_types(_):
     assert internal_fn.call_count == 4
     assert time.sleep.call_count == 3
     assert e.value.response.status_code == 500
-
-
-@unittest.mock.patch("time.sleep", return_value=None)
-@unittest.mock.patch.dict(
-    "os.environ",
-    {
-        "FOLIOCLIENT_MAX_SERVER_ERROR_RETRIES": "2",
-    },
-)
-def test_handles_failthrough_on_retry_count(_):
-
-    internal_fn = unittest.mock.Mock(
-        return_value="test",
-        side_effect=[
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=502)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=503)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=504)),
-            httpx.HTTPStatusError("test", request=None, response=SimpleNamespace(status_code=500)),
-            "test",
-        ],
-    )
-
-    with pytest.raises(httpx.HTTPStatusError) as e:
-        retry_on_server_error(internal_fn)()
-
-    assert internal_fn.call_count == 3
-    assert time.sleep.call_count == 2
-    assert e.value.response.status_code == 504
